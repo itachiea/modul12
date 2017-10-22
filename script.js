0e451bd310276ac9aaa0162150dab68ad977e02d
Konrad <w.konrad60@gmail.com>
2017-10-22 18:16:44 +0200

    Dodane 'dla chętnych'

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ $(function(){
     button.addEventListener('click', function() {
         getJoke();
     });
-
+    getJoke();      // <-- "dla chętnych"
+                    //  Zastanów się, jak zmodyfikować skrypt, aby przy pierwszym wejściu na stronę nie trzeba 
+                    //  było wciskać przycisku pobierania dowcipu. Innymi słowy: zastajemy stronę z już 
+                    //  wygenerowanym dowcipem. Podpowiem, że można to zrobić, dopisując jedną linijkę kodu.
 
 })
